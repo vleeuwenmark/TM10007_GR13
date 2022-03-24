ee8870847fa88aa15037d97ba6a1b4f1847905e6
vleeuwenmark <m.f.c.vanleeuwen@student.tudelft.nl>
2022-03-24 13:57:42 +0100

    step 1

--- adni/__init__.py
+++ adni/__init__.py
@@ -1,1 +1,12 @@
+import load_data
+import hdf5_to_csv
 
+input_directory = 'adni/ADNI_radiomicFeatures.csv'
+
+
+def main():
+    data = load_data(input_directory)
+
+
+if __name__ == "__main__":
+    main()
